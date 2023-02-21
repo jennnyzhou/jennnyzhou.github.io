85c5c0765e6e268984b262bf2b2c2c8c420e0327
jennnyzhou <jzhou@newschool.edu>
2023-02-21 14:08:27 -0500

    added stories on homepage

---
+++ project-1/home.js
@@ -0,0 +1,6 @@
+
+	jQuery(document).ready(function () {
+		jQuery(".container-wrap").append(
+			"<ul class='circles'><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>"
+		);
+	}); 
