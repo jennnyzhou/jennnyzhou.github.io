ef0149e860051af0d0a0fc10717100b0c9fc9e0d
jennnyzhou <jzhou@newschool.edu>
2023-03-30 01:58:04 -0400

    PROJECT 2

---
+++ project-2/assets/dough.js
@@ -0,0 +1,35 @@
+// Function to render your items
+const renderItems = (collection) => {
+	// The `ul` where the items will be inserted
+	const collectionList = document.getElementById('collection')
+
+	// Filter the collection to only include items with favourite = "dough"
+	const meatItems = collection.filter(item => item.favourite === "dough")
+
+	// Loop through each meat item in the filtered array
+	meatItems.forEach(item => {
+		const listItem = document.createElement('li') // Make the `li`
+
+		// This can get annoying, so we can use “template literals” instead
+		const itemDetails =
+			`
+				<p>${item.name}</p>
+				<p>${item.mustorder}</p>
+				
+				<p>${item.favouritespot}</p>
+				<p>${item.location}</p>
+				<p>${item.significance}</p>
+			`
+		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+
+		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
+	})
+}
+
+// Fetch gets your JSON file…
+fetch('assets/script.json')
+	.then(response => response.json())
+	.then(collection => {
+		// And passes the data to the function, above!
+		renderItems(collection.reverse()) // In reverse order
+	})
